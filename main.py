08554c2a0bec33328ef0024d951fdc9b362ad2f4
dilmuhammad <dizenstudios@gmail.com>
2026-08-29 14:48:55 +0500

    ending soon

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from routers import main_router
 from frontend import router as frontend_router
+import os
+from fastapi.responses import FileResponse
+from services import user_dependency
 
 
 app = FastAPI(title="Subscription maganer", description="This api for monthly subscription manager app", version="0.0.1")
@@ -21,5 +24,19 @@ app.include_router(frontend_router)
 def health():
     return {"health":"good"}
 
+
+@app.get('/db', summary="ADMIN ACCESS ONLY !!!")
+async def download_db(user: user_dependency):
+    if not user.get("role") == "admin":
+        raise HTTPException(status_code=404,detail="Only admin access")
+    try:
+        return FileResponse(
+            path=os.path.join("musha.db"),
+            filename="musha.db",
+            media_type="application/octet-stream",
+            content_disposition_type="attachment"
+        )
+    except:
+        raise HTTPException(status_code=404, detail="Database not found")
 
 # Dilmuhammad Abdukodirov 2026
